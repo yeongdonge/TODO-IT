b4455bfed501752430e21156885f4e7f822a3518
yeongdonge <parrineau@gmail.com>
2022-08-15 21:42:53 +0900

    기본 기능 구현

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" v-on:click="clearAllTodo">Clear All</span>
+  </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        clearAllTodo: function() {
+            localStorage.clear();
+        }
+    }
+
+}
+</script>
+
+<style scpoped>
+.clearAllContainer {
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 0 auto;
+}
+.clearAllBtn {
+    color: #e20303;
+    display: block;
+}
+
+
+</style>
